3c568c5dbce392a8b6da44a483ed8299da751cb2
alamindjp <alaminhossaindjp@gmail.com>
2022-04-05 03:22:35 +0600

    data load complete

--- src/Components/ReviewCard/ReviewCard.js
+++ src/Components/ReviewCard/ReviewCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './ReviewCard.css'
 
 const ReviewCard = ({ reviews }) => {
-    const { name, body, image, email } = reviews;
+    const { name, body, image, ratting } = reviews;
     return (
         <div className="review-card">
             <div className='card-title'>
                 <img src={image} alt="" />
                 <div className='name-container'>
                     <h3>{name}</h3>
-                    <small>Ratting:  out of 5</small>
+                    <small>Ratting: {ratting} out of 5</small>
                 </div>
             </div>
             <span>comment: </span><p>{body}</p>
